44ae6bdc682483236c0bf735df86d2198e464dc3
andyhuangdev <ah052691@icloud.com>
2024-06-25 11:44:54 +0800

    create and insert database

--- backend/banks/models.py
+++ backend/banks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Bank(models.Model):
+  code = models.CharField(max_length=8,unique=True)
+  name = models.CharField(max_length=32)
+
+class Branch(models.Model):
+  bank = models.ForeignKey(Bank, on_delete=models.CASCADE, related_name="branches")
+  code = models.CharField(max_length=8,unique=True)
+  name = models.CharField(max_length=32)
+  address = models.TextField()
+  phone = models.CharField(max_length=16)
